d17813435081d2db35dd470104f7f49a6992f883
adeschene <alecdeschene@gmail.com>
2021-02-14 23:01:26 -0800

    fix heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   
+  get '/home', to: 'pages#home', as: 'home'
   get '/menu', to: 'pages#menu', as: 'menu'
   get '/community', to: 'pages#community', as: 'community'
   get '/catering', to: 'pages#catering', as: 'catering'
